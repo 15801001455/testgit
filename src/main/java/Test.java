40000ba3a7925dade5a702bf5768d593b88dd715
lisi <lisi@git.com>
2019-09-18 15:33:49 +0800

    李四增加了一行

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("修改了git本地提交的用户名和密码");
+		System.out.println("李四增加一行");
     }
 }
