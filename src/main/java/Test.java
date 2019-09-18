e50bfa6f590f14ea0f437689e7911ad627300813
zhangsan <zhangsan@git.com>
2019-09-18 15:47:04 +0800

    目前在develop分支

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("aaa");
         System.out.println("修改了git本地提交的用户名和密码");
 		System.out.println("李四增加一行");
+        System.out.println("切换到develop分支");
     }
 }
